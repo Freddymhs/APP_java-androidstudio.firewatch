056088349b35a8bd3a461983ab79849a819f8444
Freddy Huaylla Silvestre <hyliankz@gmail.com>
2018-11-13 14:59:24 -0300

    pantalla splash lista , acomodar layouts

---
+++ app/src/main/java/com/example/hyliann/firewatchdesarrollo/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.hyliann.firewatchdesarrollo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+    private final int DURACION_SPLASH = 1100;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);//generico
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//oritentacion
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//fullscren
+
+        setContentView(R.layout.activity_splash);//generico
+
+        new Handler().postDelayed(new Runnable() {//metodo PostDelayed , efecto splash
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+
+        }, DURACION_SPLASH);
+    }
+}
